b54399ca14e481f608b2f1b7541404b9fa8eeb13
chenxch <chenxch@ffcs.cn>
2020-08-07 19:14:39 +0800

    修改脚手架

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,34 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <div>
+      <input v-model="text"/><button @click="sure">传给父应用</button>
+    </div>
     <HelloWorld msg="我是微应用"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data(){
+    return{
+      text: ''
+    }
+  },
+  methods:{
+    sure(){
+        if(this.text){
+          const state = { text:this.text};
+            // const actions = this.$initGlobalState(state);
+            console.log(this.$action);
+            this.$action.setGlobalState(state);
+        }
+      }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,33 +1,45 @@
+/*
+ * @Descripttion: 
+ * @Author: chenxch
+ * @Date: 2020-07-27 09:07:18
+ */
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
 
 Vue.config.productionTip = false
 
 let instance = null
 
-  function render () {
-    instance = new Vue({
-  render: h => h(App),
-}).$mount('#app')
+function render () {
+  instance = new Vue({
+    store,
+    render: h => h(App)
+  }).$mount('#app')
+}
 
-  }
+if (!window.__POWERED_BY_QIANKUN__) {
+  render()
+}
 
-  if (!window.__POWERED_BY_QIANKUN__) {
-    render()
-  }
+export async function bootstrap () {
+  // eslint-disable-next-line no-console
+  console.log('vue app bootstraped')
+}
 
-  export async function bootstrap () {
-    // eslint-disable-next-line no-console
-    console.log('vue app bootstraped')
-  }
+export async function mount (props) {
+  // eslint-disable-next-line no-console
+  console.log('props from main framework', props)
+  props.onGlobalStateChange((state, prevState) => {
+    console.log(state, prevState);
+  },true);
+  const state = {qq:12333}
+  props.setGlobalState(state);
+  Vue.prototype.$action = props;
+  render()
+}
 
-  export async function mount (props) {
-    // eslint-disable-next-line no-console
-    console.log('props from main framework', props)
-    render()
-  }
-
-  export async function unmount () {
-    instance.$destroy()
-    instance = null
-  }
+export async function unmount () {
+  instance.$destroy()
+  instance = null
+}
