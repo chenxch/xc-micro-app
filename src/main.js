58735fbd45e2d44ddd813f435e42a330bdfeea82
chenxch <chenxch@ffcs.cn>
2020-08-11 01:05:31 +0800

    安装插件

--- src/main.js
+++ src/main.js
@@ -1,33 +1,37 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
 let instance = null
 
-  function render () {
-    instance = new Vue({
-  render: h => h(App),
-}).$mount('#app')
+function render () {
+  instance = new Vue({
+    router,
+    store,
+    render: h => h(App)
+  }).$mount('#app')
 
-  }
+}
 
-  if (!window.__POWERED_BY_QIANKUN__) {
-    render()
-  }
+if (!window.__POWERED_BY_QIANKUN__) {
+  render()
+}
 
-  export async function bootstrap () {
-    // eslint-disable-next-line no-console
-    console.log('vue app bootstraped')
-  }
+export async function bootstrap () {
+  // eslint-disable-next-line no-console
+  console.log('vue app bootstraped')
+}
 
-  export async function mount (props) {
-    // eslint-disable-next-line no-console
-    console.log('props from main framework', props)
-    render()
-  }
+export async function mount (props) {
+  // eslint-disable-next-line no-console
+  console.log('props from main framework', props)
+  render()
+}
 
-  export async function unmount () {
-    instance.$destroy()
-    instance = null
-  }
+export async function unmount () {
+  instance.$destroy()
+  instance = null
+}
